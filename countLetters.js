08b5706d05c5c8018ca3010348b393184d3dac18
Jotham Yelle <jothamyelle@hotmail.com>
2018-08-01 03:22:19 +0000

    Added the program outline in pseudocode

---
+++ countLetters.js
@@ -0,0 +1,13 @@
+// objective: return all the unique characters that exist in a string passed into the function.  The program should also report back how many instances of each letter were found in the string.
+
+// a countLetters function that takes a string as a parameter that returns an object
+    // make an empty object that we'll fill
+    // ignore spaces in the passed-in string
+    // for each of the characters in the string
+        // if the letter has already been seen
+            // add one to the count for that letter in the object
+        // otherwise
+            // add that letter to the object and set its count to one
+    // return the object
+
+// display the object to the user
